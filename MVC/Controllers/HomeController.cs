26ab61eb2a2ed6495c51aa20ba25028fb5035540
samavenukumar <samavenukumar@gmail.com>
2016-12-02 15:22:59 -0600

    Add project files.

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public RedirectToRouteResult RedirectToRoute()
+        {
+            return RedirectToRoute("default");
+        }
+        public ViewResult ActionDemo()
+        {
+            return View();
+        }
+        public PartialViewResult PartialDemo()
+        {
+            return PartialView();
+        } 
+        public RedirectResult RedirectToIndex()
+        {
+
+           //return View();
+           return Redirect("Index");
+        }
+        public JsonResult JsonResultDemo()
+        {
+            userData obj = new userData()
+            {
+                UserName = "Test name",
+                email = "testmail@test.com",
+                phone = "123456789"
+            };
+            return Json(obj,JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ MVC/Models/userData.cs
@@ -0,0 +1,13 @@
+﻿namespace MVC.Controllers
+{
+    internal class userData
+    {
+        public userData()
+        {
+        }
+
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string UserName { get; set; }
+    }
+}
